89f51660544ffda7454f4d16329ed0ca36299629
Charly-privat <charly74cp00@gmail.com>
2022-01-26 14:24:12 +0100

    initialisation projet

---
+++ src/main/java/com/MSPRSecuServeur/MsprSecuServeur/MsprSecuServeurApplication.java
@@ -0,0 +1,13 @@
+package com.MsprSecuServeur.msprSecuServeur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprSecuServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsprSecuServeurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MSPRSecuServeur/MsprSecuServeur/MsprSecuServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MsprSecuServeur.msprSecuServeur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsprSecuServeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
