8eac84bef9cd91ab24526cd5cd29e4a8424e0176
Charly-privat <charly74cp00@gmail.com>
2022-02-16 09:19:48 +0100

    getLogin

--- src/main/java/com/MSPRSecuServeur/MsprSecuServeur/MsprSecuServeurApplication.java
+++ src/main/java/com/MSPRSecuServeur/MsprSecuServeur/MsprSecuServeurApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsprSecuServeurApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsprSecuServeurApplication.class, args);
 	}
-
 }
