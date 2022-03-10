c8e447757b2bb27640312e7cd607b64718ae93ff
Charly-privat <charly74cp00@gmail.com>
2022-03-10 11:21:34 +0100

    mis à jour du controller + ajout d'un envoie de mail

--- src/main/java/com/MSPRSecuServeur/MsprSecuServeur/MsprSecuServeurApplication.java
+++ src/main/java/com/MSPRSecuServeur/MsprSecuServeur/MsprSecuServeurApplication.java
@@ -1,4 +1,4 @@
-package com.MsprSecuServeur.msprSecuServeur;
+package com.MSPRSecuServeur.MsprSecuServeur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
