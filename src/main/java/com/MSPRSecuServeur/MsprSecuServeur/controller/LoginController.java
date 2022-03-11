ba587f86a253d5e9a0feb61066f3589faff8122a
Charly-privat <charly74cp00@gmail.com>
2022-03-11 14:53:49 +0100

    envoie de mail via POST. mise à jour de l'URL pour les logins.

--- src/main/java/com/MSPRSecuServeur/MsprSecuServeur/controller/LoginController.java
+++ src/main/java/com/MSPRSecuServeur/MsprSecuServeur/controller/LoginController.java
@@ -1,22 +1,18 @@
 package com.MSPRSecuServeur.MsprSecuServeur.controller;
 
 import com.MSPRSecuServeur.MsprSecuServeur.model.User;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController()
+@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping
+    @GetMapping("/")
     public User getLogin(){
         return new User(1, "charly", "privat");
     }
 
-    @PostMapping
+    @PostMapping("/")
     public Boolean postLogin(@RequestBody User user){
         return true;
     }
